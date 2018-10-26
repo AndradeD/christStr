7414341744be2980b1b76051c7d24403ea80fc15
Daniel Andrade <daniel.quintana@geohub.com.br>
2018-10-26 14:17:51 -0300

    more changes node to accept vue

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose');
 const config = require('../src/config');
 const cors = require('cors');
 
-// const Product = require('./models/product');
-// const Customer = require('./models/customer');
-// const Order = require('./models/order');
+const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 const indexRouter = require('./routes/index');
 const productRoute = require('./routes/product-route');
@@ -18,8 +18,8 @@ const orderRoute = require('./routes/order-route');
 mongoose.connect(config.connectionString);
 
 const app = express();
-// const router = express.Router();
-app.use(cors());
+const router = express.Router();
+// app.use(cors());
 
 app.use(bodyParser.json({
     limit: '15mb'
@@ -27,15 +27,15 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded( { extended: false } ));
 
 //Habilita o CORS
-// app.use(function(req,res,next){
-//     res.header('Access-Control-Allow-Origin','*');
-//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-//     res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
-//     next();
-// });
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 
-// app.use('/', indexRouter);
+//  app.use('/', indexRouter);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
 app.use('/orders',orderRoute);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/', (req, res, next) => {
-//     res.sendFile(__dirname + '/views/' + 'index.html');
-// })
+router.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/views/' + 'index.html');
+})
 
 module.exports = router;
