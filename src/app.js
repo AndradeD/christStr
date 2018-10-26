b1f7b458de03bfba050c9599369b05dc7a7608e2
Daniel Andrade <daniel.quintana@geohub.com.br>
2018-10-26 14:12:06 -0300

    update node to accept vue-cli

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('../src/config');
+const cors = require('cors');
 
-const Product = require('./models/product');
-const Customer = require('./models/customer');
-const Order = require('./models/order');
+// const Product = require('./models/product');
+// const Customer = require('./models/customer');
+// const Order = require('./models/order');
 
 const indexRouter = require('./routes/index');
 const productRoute = require('./routes/product-route');
@@ -17,11 +18,8 @@ const orderRoute = require('./routes/order-route');
 mongoose.connect(config.connectionString);
 
 const app = express();
-const router = express.Router();
-
-app.use(express.static('src/views'));
-app.use(express.static('src/scripts'));
-app.use(express.static('src/templatesPage'));
+// const router = express.Router();
+app.use(cors());
 
 app.use(bodyParser.json({
     limit: '15mb'
@@ -29,14 +27,15 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded( { extended: false } ));
 
 //Habilita o CORS
-app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+// app.use(function(req,res,next){
+//     res.header('Access-Control-Allow-Origin','*');
+//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+//     res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+// });
 
-app.use('/', indexRouter);
+
+// app.use('/', indexRouter);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
 app.use('/orders',orderRoute);
